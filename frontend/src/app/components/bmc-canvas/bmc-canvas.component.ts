a3ede34b995e5dfbcb047e1f83dba935e9b158bc
nicolashimmelmann <nhweb@mailbox.org>
2024-05-12 23:55:17 +0200

    feat: subtitles for areas

--- frontend/src/app/components/bmc-canvas/bmc-canvas.component.ts
+++ frontend/src/app/components/bmc-canvas/bmc-canvas.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { CanvasAreaComponent } from './components/canvas-area/canvas-area.component';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-bmc-canvas',
   templateUrl: './bmc-canvas.component.html',
   styleUrls: ['./bmc-canvas.component.scss'],
   standalone: true,
-  imports: [CanvasAreaComponent],
+  imports: [CommonModule, TranslateModule, CanvasAreaComponent],
 })
-export class BmcCanvasComponent {}
+export class BmcCanvasComponent {
+  protected areas = [
+    'area-partners',
+    'area-activities',
+    'area-resources',
+    'area-propositions',
+    'area-relationships',
+    'area-channels',
+    'area-customersegments',
+    'area-coststructure',
+    'area-revenuestreams',
+  ];
+}
--- frontend/src/app/components/bmc-canvas/components/canvas-area/canvas-area.component.ts
+++ frontend/src/app/components/bmc-canvas/components/canvas-area/canvas-area.component.ts
@@ -11,4 +11,7 @@ import { CardModule } from 'primeng/card';
 export class CanvasAreaComponent {
   @Input()
   name: string = '';
+
+  @Input()
+  description: string = '';
 }
