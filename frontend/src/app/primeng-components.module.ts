13426bf506f7a4d01b9a14b17f57ac42be348866
nicolashimmelmann <nhweb@mailbox.org>
2024-05-20 17:07:23 +0200

    feat: minor additions

--- frontend/src/app/primeng-components.module.ts
+++ frontend/src/app/primeng-components.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { CalendarModule } from 'primeng/calendar';
+import { ToolbarModule } from 'primeng/toolbar';
 
 /**
  * A module which imports all required primeng components.
  */
 @NgModule({
-  imports: [InputTextModule, ButtonModule, CalendarModule],
-  exports: [InputTextModule, ButtonModule, CalendarModule],
+  imports: [InputTextModule, ButtonModule, CalendarModule, ToolbarModule],
+  exports: [InputTextModule, ButtonModule, CalendarModule, ToolbarModule],
 })
 export class PrimeNGComponentsModule {}
