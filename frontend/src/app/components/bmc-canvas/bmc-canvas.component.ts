d2a04a5c4c7b314c228e8ecc5739b25e6b2d41e2
nicolashimmelmann <nhweb@mailbox.org>
2024-05-21 21:22:12 +0200

    fix: use enum for entities

--- frontend/src/app/components/bmc-canvas/bmc-canvas.component.ts
+++ frontend/src/app/components/bmc-canvas/bmc-canvas.component.ts
@@ -1,25 +1,16 @@
-import { Component } from '@angular/core';
-import { CanvasAreaComponent } from './components/canvas-area/canvas-area.component';
-import { CommonModule } from '@angular/common';
-import { TranslateModule } from '@ngx-translate/core';
+import { Component } from "@angular/core";
+import { CanvasAreaComponent } from "./components/canvas-area/canvas-area.component";
+import { CommonModule } from "@angular/common";
+import { TranslateModule } from "@ngx-translate/core";
+import { BmcEntity } from "src/app/core/models/bmc-entities.model";
 
 @Component({
-  selector: 'app-bmc-canvas',
-  templateUrl: './bmc-canvas.component.html',
-  styleUrls: ['./bmc-canvas.component.scss'],
+  selector: "app-bmc-canvas",
+  templateUrl: "./bmc-canvas.component.html",
+  styleUrls: ["./bmc-canvas.component.scss"],
   standalone: true,
   imports: [CommonModule, TranslateModule, CanvasAreaComponent],
 })
 export class BmcCanvasComponent {
-  protected areas = [
-    'area-partners',
-    'area-activities',
-    'area-resources',
-    'area-propositions',
-    'area-relationships',
-    'area-channels',
-    'area-customersegments',
-    'area-coststructure',
-    'area-revenuestreams',
-  ];
+  protected entities = Object.keys(BmcEntity);
 }
