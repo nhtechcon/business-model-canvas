71be2c1623df9f1be703cb9c1a8fca17b8ca28ac
nicolashimmelmann <nhweb@mailbox.org>
2024-06-25 22:14:53 +0200

    fix: namings

--- frontend/src/app/store/actions/canvas-list.actions.ts
+++ frontend/src/app/store/actions/canvas-list.actions.ts
@@ -2,33 +2,35 @@ import { createAction, props } from "@ngrx/store";
 import { BasicBmcCanvasInfo } from "src/app/core/models/bmc-canvas.model";
 
 export const createCanvas = createAction(
-  "[Canvas List] Create a new canvas",
+  "[canvas-list] createCanvas",
   props<{ name: string }>()
 );
 
 export const createCanvasFailure = createAction(
-  "[Canvas List] Creating new canvas failed",
+  "[canvas-list] createCanvasFailure",
   props<{ error: any }>()
 );
 
 export const addCanvas = createAction(
-  "[Canvas List] Add Canvas to list",
+  "[canvas-list] addCanvas",
   props<{ canvas: BasicBmcCanvasInfo }>()
 );
 
 export const removeCanvas = createAction(
-  "[Canvas List] Remove Canvas from list",
+  "[canvas-list] removeCanvas",
   props<{ id: string }>()
 );
 
-export const startLoadCanvases = createAction("[Canvas List] Load Canvases");
+export const startLoadCanvases = createAction(
+  "[canvas-list] startLoadCanvases"
+);
 
 export const loadCanvasesSuccess = createAction(
-  "[Canvas List] Load Canvases Success",
+  "[canvas-list] loadCanvasesSuccess",
   props<{ canvases: BasicBmcCanvasInfo[] }>()
 );
 
 export const loadCanvasesFailure = createAction(
-  "[Canvas List] Load Canvases Failure",
+  "[canvas-list] loadCanvasesFailure",
   props<{ error: any }>()
 );
