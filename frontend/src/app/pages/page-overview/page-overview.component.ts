c03f9ae5c3c7b18507361732d310b2ad3cef21a2
nicolashimmelmann <nhweb@mailbox.org>
2024-06-25 22:20:29 +0200

    feat: create and show canvas

--- frontend/src/app/pages/page-overview/page-overview.component.ts
+++ frontend/src/app/pages/page-overview/page-overview.component.ts
@@ -1,13 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy } from "@angular/core";
 import { Router } from "@angular/router";
-import { Store } from "@ngrx/store";
-import { Observable } from "rxjs";
+import { ofType } from "@ngrx/effects";
+import { ActionsSubject, Store } from "@ngrx/store";
+import { Observable, Subject, filter, takeUntil } from "rxjs";
 import { BasicBmcCanvasInfo } from "src/app/core/models/bmc-canvas.model";
 import { AuthService } from "src/app/core/services/auth.service";
-import {
-  createCanvas,
-  startLoadCanvases,
-} from "src/app/store/actions/canvas-list.actions";
+import * as CanvasListActions from "src/app/store/actions/canvas-list.actions";
+import * as CurrentCanvasActions from "src/app/store/actions/current-canvas.actions";
 import { LoadingState } from "src/app/store/common";
 import {
   selectAllCanvasesSorted,
@@ -19,7 +18,9 @@ import {
   templateUrl: "./page-overview.component.html",
   styleUrls: ["./page-overview.component.scss"],
 })
-export class PageOverviewComponent {
+export class PageOverviewComponent implements OnDestroy {
+  destroy$ = new Subject();
+
   canvasLoadingState$: Observable<LoadingState>;
   canvasList$: Observable<BasicBmcCanvasInfo[]>;
 
@@ -28,11 +29,12 @@ export class PageOverviewComponent {
   constructor(
     private store: Store,
     private auth: AuthService,
-    private router: Router
+    private router: Router,
+    private actions$: ActionsSubject
   ) {
     this.canvasLoadingState$ = this.store.select(selectLoadingState);
     this.canvasList$ = this.store.select(selectAllCanvasesSorted);
-    this.store.dispatch(startLoadCanvases());
+    this.store.dispatch(CanvasListActions.startLoadCanvases());
   }
 
   protected logoutClick() {
@@ -42,6 +44,29 @@ export class PageOverviewComponent {
 
   protected createNewCanvas(name: string) {
     this.newCanvasDialogVisible = false;
-    this.store.dispatch(createCanvas({ name }));
+    this.store.dispatch(CanvasListActions.createCanvas({ name }));
+    // Wait for canvas to be added
+    this.actions$
+      .pipe(
+        takeUntil(this.destroy$),
+        ofType(CanvasListActions.addCanvas),
+        filter(({ canvas }) => canvas.name === name)
+      )
+      .subscribe(({ canvas }) => {
+        this.store.dispatch(
+          CurrentCanvasActions.setCanvasData({
+            data: {
+              ...canvas,
+              entries: [],
+            },
+          })
+        );
+        this.router.navigate(["/main/canvas"]);
+      });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(null);
+    this.destroy$.complete();
   }
 }
