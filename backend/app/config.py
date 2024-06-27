31df38305594502fb8073a2a5a5a3831f474a0f5
nicolashimmelmann <nhweb@mailbox.org>
2024-06-27 21:01:45 +0200

    fix: cors

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 JWT_SECRET_KEY = "truyl_secret_key"
 JWT_ALGORITHM = "HS256"
 
-ALLOWED_CORS_ORIGINS = ["http://localhost:4200"]
+ALLOWED_CORS_ORIGINS = ["http://localhost:4200", "http://localhost"]
 
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "TRACE").lower()
